c964f959ee668753e057d5ed97c30f8fd75a1464
Derek Burnheim <derek.burnheim@hemicube.com>
2015-12-23 15:26:02 +1100

    WIP stuff

--- src/GamePlay.h
+++ src/GamePlay.h
@@ -3,14 +3,24 @@
 #include "IGameState.h"
 
 class GameController;
+class Input;
 class System;
 
 class GamePlay : public IGameState
 {
 public:
-	GamePlay(const System& system, GameController& gameController);
+	GamePlay(const System& system, GameController& gameController, Input& input);
 
-	bool HandleEvent(SDL_Event& event);
 	void Update(Uint32 ms);
 	void Draw(SDL_Renderer* renderer);
+
+private:
+	void UpdateWorld(Uint32 ms);
+	void UpdatePlayer(Uint32 ms);
+	void MoveCamera(Uint32 ms);
+	void UpdateEntities(Uint32 ms);
+
+	System& m_system;
+	GameController& m_gameController;
+	Input& m_input;
 };

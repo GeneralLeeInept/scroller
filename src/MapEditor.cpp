2cd632773b345f3084f5843052aee4e924a0c9d3
Derek Burnheim <derek@hemicube.com>
2018-02-21 16:08:29 +1100

    More map editor

--- src/MapEditor.cpp
+++ src/MapEditor.cpp
@@ -14,12 +14,6 @@
 #define MAPHEIGHT (SCREENTILESY * 8)
 #define MAXSCROLLX (MAPWIDTH - SCREENTILESX)
 #define MAXSCROLLY (MAPHEIGHT - SCREENTILESY)
-#define OFFSCREENTILES 16
-
-static const SDL_Rect s_offscreenRects[4] = { { 0, 0, OFFSCREENTILES, MAPHEIGHT },
-                                              { MAPWIDTH - OFFSCREENTILES, 0, OFFSCREENTILES, MAPHEIGHT },
-                                              { OFFSCREENTILES, 0, MAPWIDTH - 2 * OFFSCREENTILES, OFFSCREENTILES },
-                                              { OFFSCREENTILES, MAPHEIGHT - OFFSCREENTILES, MAPWIDTH - 2 * OFFSCREENTILES, OFFSCREENTILES } };
 
 MapEditor::MapEditor(const System& system, GameController& gameController, Input& input)
     : m_system(system)
@@ -27,16 +21,26 @@ MapEditor::MapEditor(const System& system, GameController& gameController, Input
     , m_input(input)
 {
     LoadResources(system);
-    m_tileBrush = _tile_textures.begin();
-    m_backdrop = m_backdropTextures.begin();
-//    m_mapData.Load("maps/test.map", m_system);
 
     _map_data =
     {{
-        { 0, 0, 0, 0, 0, 0, 0, 0 },
-        { 0, 0, 0, 0, 0, 0, 0, 0 },
-        { 0, 0, 0, 0, 0, 0, 0, 0 },
-        { 1, 1, 1, 1, 1, 1, 1, 1 },
+        {  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 },
+        {  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 },
+        {  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 },
+        {  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 },
+        {  0,  0,  0,  0, 25,  0,  0,  0,  0,  0,  0, 25,  0,  0,  0,  0,  0,  0,  0, 25,  0,  0,  0,  0,  0,  0,  0, 25,  0,  0,  0,  0,  0,  0, 25,  0,  0,  0,  0,  0,  0,  0, 25,  0,  0,  0 },
+        {  0,  0,  0, 25, 22,  0,  0,  0,  0,  0,  0, 22,  0,  0,  0,  0,  0,  0, 25, 22, 25,  0,  0,  0,  0,  0, 25, 22,  0,  0,  0,  0,  0,  0, 22,  0,  0,  0,  0,  0,  0, 25, 22, 25,  0,  0 },
+        {  0,  0, 25, 22, 22,  0,  0,  0,  0,  0,  0, 22,  0,  0,  0,  0,  0, 25, 22, 22, 22, 25,  0,  0,  0, 25, 22, 22,  0,  0,  0,  0,  0,  0, 22,  0,  0,  0,  0,  0, 25, 22, 22, 22, 25,  0 },
+        { 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25 },
+        { 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22 },
+        { 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22 },
+        { 22, 22, 21, 21, 21, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22 },
+        { 22, 21,  0,  0,  0, 21, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22 },
+        { 22, 21,  0,  0,  0, 21, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22 },
+        { 22, 21,  0,  0,  0, 21, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22 },
+        { 22, 21,  0,  0,  0, 21, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22 },
+        { 22, 22, 25, 25, 25, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22 },
+        { 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22 },
     }};
 }
 
@@ -51,26 +55,15 @@ void MapEditor::Update(Uint32 ms)
 
     m_scrollX = max(0, min(m_scrollX, MAXSCROLLX));
     m_scrollY = max(0, min(m_scrollY, MAXSCROLLY));
-
-    if (m_paint || m_erase)
-    {
-        m_mapData.SetTile(m_activeLayer, m_cursorX + m_scrollX, m_cursorY + m_scrollY, m_paint ? *m_tileBrush : nullptr);
-    }
-
-    if (m_brushHintShowTime != 0)
-    {
-        m_brushHintShowTime -= ms;
-
-        if (m_brushHintShowTime < 0)
-        {
-            m_brushHintShowTime = 0;
-        }
-    }
 }
 
 void MapEditor::Draw(SDL_Renderer* renderer)
 {
     SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_NONE);
+ 
+    SDL_SetRenderDrawColor(renderer, 80, 80, 80, SDL_ALPHA_OPAQUE);
+    SDL_RenderClear(renderer);
+
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
 
     // Draw map
@@ -81,6 +74,7 @@ void MapEditor::Draw(SDL_Renderer* renderer)
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
     SDL_Rect miniMap = { 16, 475, 900, 229 };
     SDL_RenderFillRect(renderer, &miniMap);
+    draw_minimap(renderer, 16, 475, 900, 229, 0.0f, 0.0f, 1.0f);
 
     SDL_Rect tools = { 948, 16, 316, 688 };
     SDL_RenderFillRect(renderer, &tools);
@@ -89,7 +83,6 @@ void MapEditor::Draw(SDL_Renderer* renderer)
 
 void MapEditor::LoadResources(const System& system)
 {
-    m_mapData.SetBackdrop(system.LoadTexture("backdrops/spooky.png"));
     m_cursor = system.LoadTexture("mapeditor/cursor.png");
 
     vector<string> tileTextures = system.GetFilesInFolder("tiles/");
@@ -130,69 +123,6 @@ void MapEditor::check_input()
     m_paint = m_input.MouseButtonDown(SDL_BUTTON_LEFT);
     m_erase = m_input.MouseButtonDown(SDL_BUTTON_RIGHT);
 
-    if (m_input.KeyReleased(SDL_SCANCODE_LEFTBRACKET))
-    {
-        if (m_tileBrush == _tile_textures.begin())
-        {
-            m_tileBrush = _tile_textures.end();
-        }
-
-        m_tileBrush = prev(m_tileBrush);
-
-        m_brushHintShowTime = 2000;
-    }
-    else if (m_input.KeyReleased(SDL_SCANCODE_RIGHTBRACKET))
-    {
-        m_tileBrush = next(m_tileBrush);
-
-        if (m_tileBrush == _tile_textures.end())
-        {
-            m_tileBrush = _tile_textures.begin();
-        }
-
-        m_brushHintShowTime = 2000;
-    }
-
-    if (m_input.KeyReleased(SDL_SCANCODE_F1))
-    {
-        m_activeLayer = GameMap::kBackground;
-    }
-    else if (m_input.KeyReleased(SDL_SCANCODE_F2))
-    {
-        m_activeLayer = GameMap::kPlayground;
-    }
-    else if (m_input.KeyReleased(SDL_SCANCODE_F3))
-    {
-        m_activeLayer = GameMap::kForeground;
-    }
-
-    if (m_input.KeyReleased(SDL_SCANCODE_S))
-    {
-        m_mapData.Save("maps/test.map");
-    }
-    else if (m_input.KeyReleased(SDL_SCANCODE_L))
-    {
-        m_mapData.Load("maps/test.map", m_system);
-    }
-
-    if (m_input.KeyReleased(SDL_SCANCODE_B))
-    {
-        m_backdrop = next(m_backdrop);
-
-        if (m_backdrop == m_backdropTextures.end())
-        {
-            m_backdrop = m_backdropTextures.begin();
-        }
-
-        m_mapData.SetBackdrop(*m_backdrop);
-    }
-
-    //if (m_input.KeyReleased(SDL_SCANCODE_X))
-    //{
-    //    m_mapData.Save("maps/test.map");
-    //    m_gameController.GotoState(GameController::kNewGame);
-    //}
-
     if (m_input.KeyDown(SDL_SCANCODE_LEFT))
     {
         --m_scrollX;
@@ -228,14 +158,44 @@ void MapEditor::draw_map(SDL_Renderer* renderer, int x, int y, int w, int h, flo
         {
             for (uint32_t tile : row)
             {
-                SDL_Rect tile_rect = { x + (tile_x * tile_size), y + (tile_y * tile_size), tile_size, tile_size };
+                SDL_Rect tile_rect = { x + ((tile_x  - m_scrollX) * tile_size), y + ((tile_y - m_scrollY) * tile_size), tile_size, tile_size };
 
                 if (tile)
                 {
-                    TexturePtr tileTexture = _tile_textures[25];
+                    TexturePtr tileTexture = _tile_textures[tile - 1];
                     SDL_RenderCopy(renderer, tileTexture, nullptr, &tile_rect);
                 }
 
+                ++tile_x;
+            }
+
+            ++tile_y;
+            tile_x = 0;
+        }
+    }
+
+    SDL_RenderSetClipRect(m_system.GetRenderer(), nullptr);
+}
+
+void MapEditor::draw_minimap(SDL_Renderer* renderer, int x, int y, int w, int h, float cx, float cy, float zoom)
+{
+    static const int tile_size = 4;
+
+    SDL_Rect clip_rect = { x, y, w, h };
+    SDL_RenderSetClipRect(renderer, &clip_rect);
+
+    for (MapLayer& layer : _map_data)
+    {
+        int tile_x = 0;
+        int tile_y = 0;
+
+        for (MapRow& row : layer)
+        {
+            for (uint32_t tile : row)
+            {
+                SDL_Rect tile_rect = { x + (tile_x * tile_size), y + (tile_y * tile_size), tile_size, tile_size };
+                SDL_SetRenderDrawColor(renderer, tile ? 80 : 0, tile ? 80 : 0, tile ? 0 : 80, SDL_ALPHA_OPAQUE);
+                SDL_RenderFillRect(renderer, &tile_rect);
                 ++tile_x;
             }
 

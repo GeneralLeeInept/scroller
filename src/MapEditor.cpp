ea818851f43180ee0f32aa20cf0ebb6b0fee5596
Derek Burnheim <derek@hemicube.com>
2018-02-27 21:53:58 +1100

    Map editor progress

--- src/MapEditor.cpp
+++ src/MapEditor.cpp
@@ -5,27 +5,17 @@
 #include "MapEditor.h"
 #include "System.h"
 
-#define TILESIZE 64
-#define SCREENTILESX (1280 / 64)
-#define SCREENTILESY ((720 + 63) / 64)
-#define MAXCURSORX (SCREENTILESX - 1)
-#define MAXCURSORY (SCREENTILESY - 1)
-#define MAPWIDTH (SCREENTILESX * 8)
-#define MAPHEIGHT (SCREENTILESY * 8)
-#define MAXSCROLLX (MAPWIDTH - SCREENTILESX)
-#define MAXSCROLLY (MAPHEIGHT - SCREENTILESY)
-
 // Map view rect
-static SDL_Rect s_mapView = { 16, 16, 900, 427 };
+static SDL_Rect s_mapView = { 16, 16, 896, 416 };
 
 // Mini-map view rect
-static SDL_Rect s_miniMapView = { 16, 475, 900, 229 };
+static SDL_Rect s_miniMapView = { 16, 475, 896, 228 };
 
 // Selected tile
 static SDL_Rect s_selectedTileView = { 948, 16, 64, 64 };
 
 // Tile palette rect
-static SDL_Rect s_tilePaletteView = { 948, 96, 316, 608 };
+static SDL_Rect s_tilePaletteView = { 948, 96, 272, 608 };
 
 MapEditor::MapEditor(const System& system, GameController& gameController, Input& input)
     : m_system(system)
@@ -34,26 +24,12 @@ MapEditor::MapEditor(const System& system, GameController& gameController, Input
 {
     LoadResources(system);
 
-    _map_data =
-    {{
-        {  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 },
-        {  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 },
-        {  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 },
-        {  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 },
-        {  0,  0,  0,  0, 25,  0,  0,  0,  0,  0,  0, 25,  0,  0,  0,  0,  0,  0,  0, 25,  0,  0,  0,  0,  0,  0,  0, 25,  0,  0,  0,  0,  0,  0, 25,  0,  0,  0,  0,  0,  0,  0, 25,  0,  0,  0 },
-        {  0,  0,  0, 25, 22,  0,  0,  0,  0,  0,  0, 22,  0,  0,  0,  0,  0,  0, 25, 22, 25,  0,  0,  0,  0,  0, 25, 22,  0,  0,  0,  0,  0,  0, 22,  0,  0,  0,  0,  0,  0, 25, 22, 25,  0,  0 },
-        {  0,  0, 25, 22, 22,  0,  0,  0,  0,  0,  0, 22,  0,  0,  0,  0,  0, 25, 22, 22, 22, 25,  0,  0,  0, 25, 22, 22,  0,  0,  0,  0,  0,  0, 22,  0,  0,  0,  0,  0, 25, 22, 22, 22, 25,  0 },
-        { 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25, 25 },
-        { 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22 },
-        { 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22 },
-        { 22, 22, 21, 21, 21, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22 },
-        { 22, 21,  0,  0,  0, 21, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22 },
-        { 22, 21,  0,  0,  0, 21, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22 },
-        { 22, 21,  0,  0,  0, 21, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22 },
-        { 22, 21,  0,  0,  0, 21, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22 },
-        { 22, 22, 25, 25, 25, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22 },
-        { 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22 },
-    }};
+    _map_data.resize(1);
+    _map_data[0].resize((s_miniMapView.h / 4) + 1);
+    for (auto& row : _map_data[0])
+    {
+        row.resize((s_miniMapView.w / 4) + 1);
+    }
 
     SDL_ShowCursor(1);
 }
@@ -69,11 +45,10 @@ void MapEditor::Update(Uint32 ms)
 
     check_input();
 
-    m_cursorX = max(0, min(m_cursorX, MAXCURSORX));
-    m_cursorY = max(0, min(m_cursorY, MAXCURSORY));
-
-    m_scrollX = max(0, min(m_scrollX, MAXSCROLLX));
-    m_scrollY = max(0, min(m_scrollY, MAXSCROLLY));
+    int maxScrollX = (s_miniMapView.w / 4) - (s_mapView.w / 32);
+    int maxScrollY = (s_miniMapView.h / 4) - (s_mapView.h / 32);
+    m_scrollX = max(0, min(m_scrollX, maxScrollX));
+    m_scrollY = max(0, min(m_scrollY, maxScrollY));
 }
 
 void MapEditor::Draw(SDL_Renderer* renderer)
@@ -136,20 +111,34 @@ void MapEditor::LoadResources(const System& system)
 
 void MapEditor::check_input()
 {
-    m_input.GetMousePosition(&m_cursorX, &m_cursorY);
+    SDL_Point mouse_position;
+
+    m_input.GetMousePosition(&mouse_position.x, &mouse_position.y);
+
+    if (SDL_PointInRect(&mouse_position, &s_mapView))
+    {
+        m_cursorX = (mouse_position.x - s_mapView.x) / 32;
+        m_cursorY = (mouse_position.y - s_mapView.y) / 32;
+        SDL_ShowCursor(0);
+    }
+    else
+    {
+        m_cursorX = -1;
+        m_cursorY = -1;
+        SDL_ShowCursor(1);
+    }
 
     if (m_input.MouseButtonDown(SDL_BUTTON_LEFT))
     {
-        SDL_Point click = { m_cursorX, m_cursorY };
-        if (SDL_PointInRect(&click, &s_miniMapView))
+        if (SDL_PointInRect(&mouse_position, &s_miniMapView))
         {
-            m_scrollX = ((m_cursorX - s_miniMapView.x) / 4) - s_mapView.w / 64;
-            m_scrollY = ((m_cursorY - s_miniMapView.y) / 4) - s_mapView.h / 64;
+            m_scrollX = ((mouse_position.x - s_miniMapView.x) / 4) - s_mapView.w / 64;
+            m_scrollY = ((mouse_position.y - s_miniMapView.y) / 4) - s_mapView.h / 64;
         }
-        else if (SDL_PointInRect(&click, &s_tilePaletteView))
+        else if (SDL_PointInRect(&mouse_position, &s_tilePaletteView))
         {
-            int view_x = (m_cursorX - s_tilePaletteView.x - 2) / 68;
-            int view_y = (m_cursorY - s_tilePaletteView.y - 2) / 68;
+            int view_x = (mouse_position.x - s_tilePaletteView.x - 2) / 68;
+            int view_y = (mouse_position.y - s_tilePaletteView.y - 2) / 68;
             int tiles_per_row = s_tilePaletteView.w / 68;
             int tile_index = view_x + view_y * tiles_per_row;
 
@@ -159,11 +148,16 @@ void MapEditor::check_input()
                 m_selectedTileTexture = _tile_textures[tile_index];
             }
         }
-        else if (SDL_PointInRect(&click, &s_mapView) && m_selectedTile)
+        else if (m_cursorX >= 0 && m_cursorY >= 0)
         {
-            int view_x = m_scrollX + (m_cursorX - s_mapView.x) / 32;
-            int view_y = m_scrollY + (m_cursorY - s_mapView.y) / 32;
-            _map_data[0][view_y][view_x] = m_selectedTile;
+            _map_data[0][m_cursorY + m_scrollY][m_cursorX + m_scrollX] = m_selectedTile;
+        }
+    }
+    else if (m_input.MouseButtonDown(SDL_BUTTON_RIGHT))
+    {
+        if (SDL_PointInRect(&mouse_position, &s_mapView) && m_selectedTile)
+        {
+            _map_data[0][m_cursorY + m_scrollY][m_cursorX + m_scrollX] = 0;
         }
     }
 
@@ -218,6 +212,21 @@ void MapEditor::draw_map(SDL_Renderer* renderer, int x, int y, int w, int h, flo
         }
     }
 
+    if (m_cursorX >= 0 && m_cursorY >= 0)
+    {
+        SDL_Rect tile_rect = { x + m_cursorX * tile_size, y + m_cursorY * tile_size, tile_size, tile_size };
+
+        if (m_selectedTileTexture != nullptr)
+        {
+            SDL_RenderCopy(renderer, m_selectedTileTexture, nullptr, &tile_rect);
+        }
+    
+        SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
+        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 128);
+        SDL_RenderFillRect(renderer, &tile_rect);
+        SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_NONE);
+    }
+
     SDL_RenderSetClipRect(m_system.GetRenderer(), nullptr);
 }
 
@@ -250,7 +259,7 @@ void MapEditor::draw_minimap(SDL_Renderer* renderer, int x, int y, int w, int h,
 
     SDL_SetRenderDrawColor(renderer, 128, 255, 128, SDL_ALPHA_OPAQUE);
 
-    SDL_Rect zoom_rect = { x + (m_scrollX - 1) * tile_size, y + (m_scrollY - 1) * tile_size, ((s_mapView.w / 32) + 1) * tile_size, ((s_mapView.h / 32) + 1) * tile_size };
+    SDL_Rect zoom_rect = { x + m_scrollX * tile_size - 1, y + m_scrollY * tile_size - 1, (s_mapView.w / 32) * tile_size + 2, (s_mapView.h / 32) * tile_size + 2 };
     SDL_RenderDrawRect(renderer, &zoom_rect);
 
     SDL_RenderSetClipRect(renderer, nullptr);
